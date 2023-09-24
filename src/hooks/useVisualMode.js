203460632268d66b59596654bcbfd04230e85258
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-09-24 16:34:54 -0700

    Add a history and a function to move back to a previous state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -13,9 +13,21 @@ const MODE = {
 };
 
 const useVisualMode = (initial) => {
-  const [ mode, setMode ] = useState(initial || MODE.EMPTY);
+  const [history, setHistory] = useState([initial || MODE.EMPTY])
   
-  return { mode };
+  const transition = (newMode) => {
+    setHistory(prev =>[...prev, newMode]);
+  };
+
+  const back = () => {
+    setHistory(prev => [...prev.slice(0, prev.length - 1)]);
+  };
+
+  return {
+    mode: history[history.length - 1],
+    transition,
+    back
+  };
 };
 
 export default useVisualMode;
